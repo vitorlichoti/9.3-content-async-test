992102dca4c8252a0455c37b762aba161173f827
Leticia Bora <leticia.bora@gmail.com>
2021-12-07 17:21:32 -0300

    conteúdo completo

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -4,13 +4,12 @@ const { fetchCharacter } = require('../src/fetchCharacter');
 describe('Teste a função fetchCharacter', () => {
 
   it('Verifica se o nome da personagem é Wonder Woman', async () => {
-    const getCharacter = await fetchCharacter('720');
-    expect(getCharacter.name).toBe('Wonder Woman');
+    const character = await fetchCharacter('720');
+    expect(character.name).toBe('Wonder Woman');
   });
 
   it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
     const failRequest = await fetchCharacter();
-
     expect(failRequest).toEqual(new Error('You must provide an url'));
   });
 
